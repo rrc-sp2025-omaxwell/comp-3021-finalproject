079d49913146453dbe02fdb5ad1be6d903e38416
owen maxwell <omaxwell@rrc.ca>
2026-03-31 15:12:51 -0500

    feat: exposed secrets for scanning.

--- src/config/firebaseConfig.ts
+++ src/config/firebaseConfig.ts
@@ -1,7 +1,7 @@
 import { initializeApp, cert, ServiceAccount } from "firebase-admin/app";
 import { getFirestore, Firestore } from "firebase-admin/firestore";
 
-import * as serviceAccount from "./comp-3018-coding-challenge-3-firebase-adminsdk-fbsvc-479c230ea0.json";
+import * as serviceAccount from "./fake_firebase_key.json";
 
 // Initialize the Firebase app with the service account credentials
 // This step is necessary before you can use any Firebase services
